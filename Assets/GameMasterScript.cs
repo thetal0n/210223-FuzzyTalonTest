f5e4f4b10bb1beeb27cdda8020618b6251d59d73
FuzzyCodes <59210853+FuzzyCodes@users.noreply.github.com>
2021-04-29 00:29:39 -0700

    Big Edit
    1. I deleted the Plane and the Ball
    2. I Made some code that instantiated floating cubes
    -2a. I made some boundary markers that will serve to provide a sense of scale when the user makes different functions
    -2b. I hardcoded a sample function that the user should be able to edit at a later point.

--- Assets/GameMasterScript.cs
+++ Assets/GameMasterScript.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class GameMasterScript : MonoBehaviour
 {
+    private float masterX = 0;
+    private float masterY = 0;
+    public GameObject cube;
+    private Vector3 spawnPos;
+    public float boundScaling = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        masterX = -20;
+        spawnPos.x = 0;
+        spawnPos.y = 0;
+        spawnPos.z = 0;
+        spawnPos.x = masterX;
+        float b = boundScaling;
+        Instantiate(cube, new Vector3(-b, -b, 0), Quaternion.identity);
+        Instantiate(cube, new Vector3(-b, 0, 0), Quaternion.identity);
+        Instantiate(cube, new Vector3(-b, b, 0), Quaternion.identity);
+        Instantiate(cube, new Vector3(0, -b, 0), Quaternion.identity);
+        Instantiate(cube, new Vector3(0, 0, 0), Quaternion.identity);
+        Instantiate(cube, new Vector3(0, b, 0), Quaternion.identity);
+        Instantiate(cube, new Vector3(b, -b, 0), Quaternion.identity);
+        Instantiate(cube, new Vector3(b, 0, 0), Quaternion.identity);
+        Instantiate(cube, new Vector3(b, b, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        tick();
+        spawnPos.x = masterX;
+        spawnPos.y = -(masterX * masterX + 4 * masterX + 3);
+        // Instantiate a cube
+        if (spawnPos.x <= 20 && spawnPos.x >= -20 && spawnPos.y > -20 && spawnPos.y < 20)
+        {
+            Instantiate(cube, spawnPos, Quaternion.identity);
+        }
+    }
+
+    void tick()
+    {
+        masterX += .1f;
     }
 }
